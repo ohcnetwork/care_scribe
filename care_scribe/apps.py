d1eb5a535e0db7f9e71d446f529ad7cf2a6b6dc9
Shivank Kacker <kacker.shivank@gmail.com>
2024-09-28 02:03:55 +0530

    Added Scribe Feature Flags (#13)

--- care_scribe/apps.py
+++ care_scribe/apps.py
@@ -1,7 +1,11 @@
 from django.apps import AppConfig
+from care.utils.registries.feature_flag import FlagRegistry, FlagType
 
 PLUGIN_NAME = "care_scribe"
-
-
 class CareScribeConfig(AppConfig):
     name = PLUGIN_NAME
+
+    def ready(self):
+        FlagRegistry.register(FlagType.FACILITY, "SCRIBE_ENABLED")
+        FlagRegistry.register(FlagType.USER, "SCRIBE_ENABLED")
+

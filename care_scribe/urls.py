f5445c6bb71d2e6acb8fa27a3eeb6fa1d25d55e1
Shivank Kacker <kacker.shivank@gmail.com>
2025-07-15 15:32:13 +0530

    quota init

--- care_scribe/urls.py
+++ care_scribe/urls.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from care_scribe.care_scribe.viewsets.scribe_quota import ScribeQuotaViewSet
 from care_scribe.viewsets.scribe import ScribeViewset
 from care_scribe.viewsets.scribe_file import FileUploadViewSet
 
@@ -12,6 +13,7 @@ def healthy(request):
 
 router = DefaultRouter()
 router.register("scribe", ScribeViewset)
+router.register("quota", ScribeQuotaViewSet)
 router.register("scribe_file", FileUploadViewSet)
 
 urlpatterns = [

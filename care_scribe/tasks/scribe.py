771121cc221ffedb158a23c47329a14bd75b304e
Shivank Kacker <kacker.shivank@gmail.com>
2025-07-24 18:51:58 +0530

    fix edge case

--- care_scribe/tasks/scribe.py
+++ care_scribe/tasks/scribe.py
@@ -14,6 +14,7 @@ from care_scribe.settings import plugin_settings
 from google.genai import types
 from google import genai
 from google.oauth2 import service_account
+from django.db import transaction
 
 from care_scribe.utils import hash_string, remove_keys
 
@@ -53,7 +54,8 @@ def ai_client(provider=plugin_settings.SCRIBE_API_PROVIDER):
 
 
 @shared_task
-def process_ai_form_fill(external_id):
+def process_ai_form_fill(external_id, base_chunk_size=None):
+    print(f"BASE CHUNK SIZE: {base_chunk_size}")
     base_prompt = textwrap.dedent(
         """
         You will receive a patient's encounter in the form of text, audio, or image. Your task is to extract all relevant data and populate the specified form fields accordingly. Follow the instructions and rules meticulously to ensure accuracy and compliance.
@@ -173,10 +175,11 @@ def process_ai_form_fill(external_id):
     processed_fields = remove_keys(processed_fields, keys_to_remove)
 
     # divide the processed fields into chunks
-    chunk_size = 40
 
-    if api_provider == "google":
+    chunk_size = base_chunk_size or 40
+    if not base_chunk_size and api_provider == "google":
         chunk_size = 200
+
 
     processed_fields_no_keys = {f"q{i}": v for i, (k, v) in enumerate(processed_fields.items())}
 
@@ -379,6 +382,11 @@ def process_ai_form_fill(external_id):
                         )
                     except Exception as e:
                         existing_cache = None
+                        if not base_chunk_size and idx == 0:
+                            form.status = Scribe.Status.READY
+                            form.save()
+                            transaction.on_commit(lambda: process_ai_form_fill.delay(external_id, base_chunk_size=20))
+                            return
 
                 will_use_cache = existing_cache and existing_cache.usage_metadata.total_token_count > 1024
                 if will_use_cache:
